c6c6ce568e4e18d18ace797ec65e874a446c7ef1
Cristian Guzman <mono-717@hotmail.com>
2023-09-19 21:24:40 +0200

    secondCommit

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function laCajaDePandora(numero){
     return numero % 2 ===0 ? numero.toString(2)  
     : numero.toString(16)
 }
+
+function Cristian(){
+    return {
+        name:'Cristian Guzman',
+        age:31,
+        nationality:'Colombian'
+    }
+}
